ac3a0b5246370563b6a6f22e8320607f9f6c35a3
mehedi hasan <mehedicsebu@gmail.com>
2022-02-21 12:51:18 +0600

    introduction of ES6 added

---
+++ basic.js
@@ -0,0 +1,47 @@
+let deposit = 400;
+if (deposit == 400) {
+
+}
+else if (deposit != 400) {
+
+}
+else if (deposit > 400) {
+
+}
+else if (deposit < 400) {
+
+}
+else if (deposit >= 400) {
+
+}
+else if (deposit <= 400) {
+
+}
+
+//array
+const numbers = [23, 333, 25, 243, 67, 454];
+const numberCount = numbers.length();
+numbers.push(34);
+numbers.pop();
+numbers[3] = 3333;
+//check whether 222 included in the array
+if (numbers.indexOf(222) != -1) {
+
+}
+if (numbers.includes(222)) {
+
+}
+//loop
+//while for
+for (const number of numbers) {
+
+}
+
+//function
+function getFullName(first, second) {
+    const name = first + ' ' + second;
+}
+const person = getFullName('rohim', 'ali');
+//object
+const bottle = { color: 'yellow', price: 200, contain: 'water' };
+
---
+++ dom.js
@@ -0,0 +1,32 @@
+document.getElementById('addBorder').addEventListener('click', function () {
+    const borderAdd = document.getElementsByClassName('friend');;
+    for (const friend of borderAdd) {
+        friend.style.border = '2px solid red';
+    }
+})
+function addBackground() {
+    const backgroundAdd = document.getElementsByClassName('friend');;
+    for (const friend of backgroundAdd) {
+        friend.style.backgroundColor = 'yellow';
+    }
+}
+
+document.getElementById('addFriend').addEventListener('click', function () {
+    const friensList = document.getElementById('friend_container')
+    const friendss = document.createElement('div');
+    friendss.classList.add('friend');
+    friensList.appendChild(friendss)
+    friendss.innerHTML = `
+    <h3 class="friend-name">Friend-New</h3>
+            <p>Corporis dolor molestias voluptas optio harum tenetur officiis.</p>
+
+    `;
+})
+document.getElementById('colorFriendName').addEventListener('click', function () {
+    const colorFriend = document.getElementsByClassName('friend-name');
+    for (const friend of colorFriend) {
+        friend.style.color = 'red';
+    }
+
+
+})
