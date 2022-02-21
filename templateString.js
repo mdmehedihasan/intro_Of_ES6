f6d41155d0182cde830eb1ddffc3ce4c29bd40d7
mehedi hasan <mehedicsebu@gmail.com>
2022-02-21 15:37:50 +0600

    templateString added

---
+++ templateString.js
@@ -0,0 +1,9 @@
+const count = ['rubu', 'subu', 'habu', 'labu'];
+const pirya = 'o priya tumi kothay';
+const kobita = 'kobita tumi sopno charini hoye';
+const total = `This is 
+backtic and we will got it
+${pirya} and ${kobita} equation ${count.length * 3}
+
+`;
+console.log(total);
